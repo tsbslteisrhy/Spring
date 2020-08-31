31e935b1505e952f68871b2088aa06fd465d530a
bigdata1 <tsbslteisrhy@naver.com>
2020-08-31 12:49:48 +0900

    2020/08/31 Spring

---
+++ Ch03/src/main/java/kr/co/ch03/Internet.java
@@ -0,0 +1,11 @@
+package kr.co.ch03;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Internet {
+
+	public void access() {
+		System.out.println("인터넷 연결...");
+	}
+}
